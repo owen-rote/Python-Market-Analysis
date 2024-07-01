a26ddb4ef71f9855325b7900968e246d21b3236a
owen-rote <rotenbergowen@gmail.com>
2024-07-01 14:37:25 -0700

    feat: correlation heatmap generation

--- sp500 data compiler.py
+++ sp500 data compiler.py
@@ -4,10 +4,15 @@
 import bs4 as bs
 import datetime as dt
 import os
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
 import pandas as pd
 import yfinance as yf
 import json
 import requests
+
+style.use("ggplot")
 
 
 def save_sp500_tickers() -> list:
@@ -31,7 +36,7 @@ def save_sp500_tickers() -> list:
 
 
 def fetch_yahoo_data(reload_sp500=False, reload_data=False) -> None:
-    """Iterates through tickers and saves a csv of the data in stock_dfs/
+    """Iterates through tickers and saves a csv of the data in stock_dfs dir
 
     Args:
         reload_sp500 (bool, optional): Re-downloads updated s&p 500 tickers. Defaults to False.
@@ -70,7 +75,11 @@ def fetch_yahoo_data(reload_sp500=False, reload_data=False) -> None:
         else:
             print("Already have {}".format(ticker))
 
+
 def compile_data() -> None:
+    """Compiles each stock's adj close price for each
+        date into one dataframe: sp500_joined_closes.csv
+    """
     with open("sp500tickers.txt", "r") as f:
         tickers = json.load(f)
 
@@ -90,10 +99,51 @@ def compile_data() -> None:
         if count % 10 == 0:
             print(count)
 
-    print(main_df.head())
-
+    print("Compilation complete")
     main_df.to_csv("sp500_joined_closes.csv")
 
 
-compile_data()
+def visualize_data(pct_change = False) -> None:
+    """Generates a heatmap correlation table of all s&p 500 stock prices OR returns
 
+    Args:
+        pct_change (bool, optional): Generates based on returns rather than price. 
+            Returns tend to follow normal distrubution and prices don't. Defaults to False.
+    """
+    df = pd.read_csv("sp500_joined_closes.csv", parse_dates=["Date"], index_col="Date")
+
+    # Generate correlation table
+    if pct_change:
+        df_corr = df.pct_change().corr()
+    else:
+        df_corr = df.corr()
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    # =============== Generate heatmap on a grid ===============
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+
+    # Add tickmarks
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    # Add labels
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+
+    # Set range from -1 to 1
+    heatmap.set_clim(-1, 1)
+
+    plt.tight_layout
+    plt.show()
+
+
+visualize_data()
