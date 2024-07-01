4f682e09d141438b0640b7bd4234cf8ae6405ffc
owen-rote <rotenbergowen@gmail.com>
2024-06-30 23:56:54 -0700

    Add 100ma and volume charts

--- simple charting.py
+++ simple charting.py
@@ -25,7 +25,17 @@ df = pd.read_csv("spy.csv", parse_dates=True, index_col=0)
 # plt.show()
 
 # Create 100 moving average col (Avg of last 100 days' price)
-df["100ma"] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df["100ma"] = df["Adj Close"].rolling(window=100, min_periods=0).mean()
 # min_periods adjusts for first 100 rows
 
 print(df.head())
+
+#                      (size) (start pt)
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (0, 0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df["Adj Close"])
+ax1.plot(df.index, df["100ma"])
+ax2.bar(df.index, df["Volume"])
+
+plt.show()
