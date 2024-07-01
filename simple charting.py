b296118f5ef7caa63310ddd970b38a649d8c02f2
owen-rote <rotenbergowen@gmail.com>
2024-06-30 23:50:47 -0700

    Add plot options

--- simple charting.py
+++ simple charting.py
@@ -6,9 +6,26 @@ import yfinance as yf
 
 style.use("ggplot")
 
-start = dt.datetime(2020, 1, 1)
-end = dt.datetime.now()
-df = yf.download('BTC-USD', start, end)
+# Uncomment to download csv
+# =========================
+# start = dt.datetime(2020, 1, 1)
+# end = dt.datetime.now()
+# df = yf.download('SPY', start, end)
+# df.to_csv('spy.csv')
+
+df = pd.read_csv("spy.csv", parse_dates=True, index_col=0)
+
+
+# Data display options
+# ====================
+# print(df.tail())
+# print(df[["Open", "High"]].head())
+# print(df["Adj Close"].head())
+# df["Adj Close"].plot()
+# plt.show()
+
+# Create 100 moving average col (Avg of last 100 days' price)
+df["100ma"] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+# min_periods adjusts for first 100 rows
 
 print(df.head())
-
